b84b42b2850a80b061ddac80a12853094a644562
pujaayumiswari <payumiswari@gmail.com>
2023-10-07 17:20:58 +0700

    create detail artikel

--- NOESAKAN.ID/FRONTEND/src/App.tsx
+++ NOESAKAN.ID/FRONTEND/src/App.tsx
@@ -1,10 +1,14 @@
-/* eslint-disable react-hooks/exhaustive-deps */
+
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Box } from "@chakra-ui/react";
 import { Login } from "./pages/login";
 import Register from "./pages/register";
 // import DiscussGrup from "./pages/DiscussGrup";
-import Article from "./components/Article";
+import Article from "./components/Discuss";
+import Home from "./pages/home";
+import DetailProduct from "./pages/detailarticle";
+import DetailArticle from "./pages/detailarticle";
+
 
 function App() {
   return (
@@ -14,6 +18,10 @@ function App() {
           <Route
             path="/"
             element={<Home />}
+          />
+          <Route
+            path="/detailartikel"
+            element={< DetailArticle/>}
           />
         </Routes>
       </div>
--- NOESAKAN.ID/FRONTEND/src/pages/login.tsx
+++ NOESAKAN.ID/FRONTEND/src/pages/login.tsx
@@ -2,7 +2,6 @@ import FormLogin from "../components/FormLogin";
 import { Grid, GridItem } from "@chakra-ui/react";
 
 export function Login() {
- const backgroundImageUrl = 'https://images.unsplash.com/photo-1595009901108-41259ec1b3f6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80';
   return (
     <>
       <Grid   
